a42d3b14a60f6999cfaec44c39664be1ce058bde
LucoZozo <lucas.naman@gmail.com>
2021-11-10 11:24:43 +0100

    restart from 0

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure } from 'mobx';
 import './index.css';
 import App from './App';
-import { StoreProvider } from './store/Provider';
-import { Store } from './store/store';
-
-// initialize mobx
-configure({ enforceActions: 'observed' });
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider store={new Store()}>
-      <App />
-    </StoreProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
